2c9680472bced5007951a8ba3330aa31be0a2ae0
igara <igara1119@gmail.com>
2015-11-19 01:24:31 +0900

    jqueryのパスが間違っていたので修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <link href="../css/bootstrap/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="../css/chat/chat.css" rel="stylesheet" type="text/css">
-    <script type="text/javascript" src="../js/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/jQuery/jquery.min.js"></script>
     <script type="text/javascript" src="../js/bootstrap/bootstrap.min.js"></script>
 </head>
 <body>
