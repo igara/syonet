c5d6dffd19bfb7ae45991707e13db99c4e29f555
igara <igara1119@gmail.com>
2015-12-27 00:57:27 +0900

    一時的にbreadの記載をドキュメント用に作成したAngularJSのテンプレートを使用させる

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" ng-app="app">
 <head>
     <link href="../css/lib/onsenui/onsenui.css" rel="stylesheet" type="text/css">
     <link href="../css/lib/onsenui/onsen-css-components-dark-theme.css" rel="stylesheet" type="text/css">
-    <script type="text/javascript" src="../js/lib/systemjs/system.js"></script>
     <script type="text/javascript" src="../js/lib/angular/angular.min.js"></script>
     <script type="text/javascript" src="../js/lib/onsenui/onsenui.min.js"></script>
-    <script>
-        ons.bootstrap();
-    </script>
+    <script type="text/javascript" src="../js/lib/onsenui/angular-onsenui.min.js"></script>
+    <script type="text/javascript" src="http://localhost:8000/js/docs/screen/common/controller/docklist_controller.js"></script>
 </head>
 <body>
-@include('welcome.common.header')
-@include('welcome.common.menu')
-@include('welcome.common.footer')
+
+<ons-sliding-menu var="menu"
+    menu-page="@{{hostName}}/docs/screen/common/design/design_menu.html"
+    main-page="@{{hostName}}/docs/screen/common/design/design_tabbar.html"
+    side="left" type="push" max-slide-distance="200px">
+</ons-sliding-menu>
 </body>
 </html>
