024825d1064f090ce2cabfd674b31ecdeed8e4bb
igara <igara1119@gmail.com>
2016-01-04 15:40:13 +0900

    不要なコントローラの削除と新規コントローラーの作成

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Home\HomeController@topAction');
 
-Route::get('/debug/phantomjs', 'Debug\PhantomJSController@indexAction');
-
-Route::get('/chat/', 'Chat\ChatController@indexAction');
