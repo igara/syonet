6980578edcc4c5fbcd83b2587159f9245b3e4835
igara <igara1119@gmail.com>
2016-01-16 17:11:42 +0900

    urlパターンをroutesに直接書かないように変更

---
+++ app/Http/PathInfo.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http;
+
+/**
+ * syonetに用いるURLやパスを用いるクラス
+ */
+class PathInfo {
+
+    /**
+     * rootページのurl情報を取得する
+     * 
+     * @return rootのページのurl情報
+     */
+    public function getRootUrlInfo() {
+        return [
+            'TOP_SCREEN' => [
+                'URL' => '/'
+            ]
+        ];
+    }
+
+    /**
+     * tabbarのurl情報を取得する
+     * 
+     * @return tabbarのurl情報
+     */
+    public function getTabbarUrlInfo() {
+        return [
+            'TABBAR' => [
+                'URL' => '/tabbar/tabbar_top'
+            ]
+        ];
+    }
+
+    /**
+     * sidemenuのurl情報を取得する
+     * 
+     * @return sidemenuのurl情報
+     */
+    public function getSideMenuUrlInfo() {
+        return [
+            'SIDEMENU' => [
+                'URL' => '/sidemenu/sidemenu_top'
+            ]
+        ];
+    }
+
+    /**
+     * homeタブで呼ばれるページのurl情報を取得する
+     * 
+     * @return homeタブで呼ばれるページのurl情報
+     */
+    public function getHomeTabUrlInfo() {
+        return [
+            'TOP_SCREEN' => [
+                'URL' => '/'
+            ]
+        ];
+    }
+
+    /**
+     * Contentsタブで呼ばれるページのurl情報を取得する
+     * 
+     * @return Contentsタブで呼ばれるページのurl情報
+     */
+    public function getContentsTabUrlInfo() {
+        return [
+            'TABBAR_CONTENTS_TOP' => [
+                'URL' => '/tabbar/navigation/contents/contents_top'
+            ],
+            'TABBAR_CONTENTS_CHAT' => [
+                'URL' => '/tabbar/navigation/contents/contents_chat'
+            ]
+        ];
+    }
+
+    /**
+     * Docsタブで呼ばれるページのurl情報を取得する
+     * 
+     * @return Docsタブで呼ばれるページのurl情報
+     */
+    public function getDocsTabUrlInfo() {
+        return [
+            'TABBAR_DOCS_TOP' => [
+                'URL' => '/tabbar/navigation/docs/docs_top'
+            ],
+            'TABBAR_DOCS_SCREENLIST' => [
+                'URL' => '/tabbar/navigation/docs/screenlist'
+            ]
+        ];
+    }
+
+    /**
+     * Devタブで呼ばれるページのurl情報を取得する
+     * 
+     * @return Devタブで呼ばれるページのurl情報
+     */
+    public function getDevTabUrlInfo() {
+        return [
+            'TABBAR_DEV_TOP' => [
+                'URL' => '/tabbar/navigation/dev/dev_top'
+            ]
+        ];
+    }
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,32 +1,60 @@
 <?php
 
+use App\Http\PathInfo;
+
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
-Route::get('/', 'Tabbar\Navigation\Home\HomeController@topAction');
+// PathInfoクラスをインスタンス化したものを取得する
+$pathInfo = new PathInfo();
 
-Route::get('/tabbar/navigation/contents/contents_top', 'Tabbar\Navigation\Contents\ContentsController@topAction');
-Route::get('/tabbar/navigation/contents/contents_chat', 'Tabbar\Navigation\Contents\ContentsController@chatAction');
+// rootページのurl情報
+$rootUrlInfo = $pathInfo->getRootUrlInfo();
+
+// tabbarのurl情報
+$tabbarUrlInfo = $pathInfo->getTabbarUrlInfo();
+
+$sidemenuUrlInfo = $pathInfo->getSideMenuUrlInfo();
+
+// homeタブのurl情報
+$homeTabUrlInfo = $pathInfo->getHomeTabUrlInfo();
+
+// contentsタブのurl情報
+$contentsTabUrlInfo = $pathInfo->getContentsTabUrlInfo();
+
+// docsタブのurl情報
+$docsTabUrlInfo = $pathInfo->getDocsTabUrlInfo();
+
+// devタブのurl情報
+$devTabUrlInfo = $pathInfo->getDevTabUrlInfo();
 
 
-Route::get('/tabbar/navigation/docs/docs_top', 'Tabbar\Navigation\Docs\DocsController@topAction');
-Route::get('/tabbar/navigation/docs/screenlist', 'Tabbar\Navigation\Docs\DocsController@screenListAction');
+Route::get($rootUrlInfo['TOP_SCREEN']['URL'], 'Tabbar\Navigation\Home\HomeController@topAction');
 
+// タブバー
+Route::get($tabbarUrlInfo['TABBAR']['URL'], 'Tabbar\TabbarController@topAction');
 
-Route::get('/tabbar/navigation/dev/dev_top', 'Tabbar\Navigation\Dev\DevController@topAction');
+// サイドメニュー
+Route::get($sidemenuUrlInfo['SIDEMENU']['URL'], 'SideMenu\SideMenuController@topAction');
 
+// contentsタブ
+Route::get($contentsTabUrlInfo['TABBAR_CONTENTS_TOP']['URL'], 'Tabbar\Navigation\Contents\ContentsController@topAction');
+Route::get($contentsTabUrlInfo['TABBAR_CONTENTS_CHAT']['URL'], 'Tabbar\Navigation\Contents\ContentsController@chatAction');
 
-Route::get('/tabbar/tabbar_top', 'Tabbar\TabbarController@topAction');
+// docsタブ
+Route::get($docsTabUrlInfo['TABBAR_DOCS_TOP']['URL'], 'Tabbar\Navigation\Docs\DocsController@topAction');
+Route::get($docsTabUrlInfo['TABBAR_DOCS_SCREENLIST']['URL'], 'Tabbar\Navigation\Docs\DocsController@screenListAction');
 
-Route::get('/sidemenu/sidemenu_top', 'SideMenu\SideMenuController@topAction');
+// devタブ
+Route::get($devTabUrlInfo['TABBAR_DEV_TOP']['URL'], 'Tabbar\Navigation\Dev\DevController@topAction');
 
 
 Route::get('/debug/phpinfo', 'Debug\ServerAndClientInfoController@phpinfoAction');
