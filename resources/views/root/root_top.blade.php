a1639c18a0866726c8c42f9795ca529a2015df3a
igara <igara1119@gmail.com>
2016-01-17 16:00:36 +0900

    明るいデザインにする

--- resources/views/root/root_top.blade.php
+++ resources/views/root/root_top.blade.php
@@ -4,7 +4,7 @@
     <title>SyoNet!</title>	
     <meta name="description" content="Web・Networkなどの技術サイトです。">
     <link href="/css/lib/onsenui/onsenui.css" rel="stylesheet" type="text/css">
-    <link href="/css/lib/onsenui/onsen-css-components-dark-theme.css" rel="stylesheet" type="text/css">
+    <link href="/css/lib/onsenui/onsen-css-components-blue-basic-theme.css" rel="stylesheet" type="text/css">
     <script type="text/javascript" src="/js/lib/angular/angular.min.js"></script>
     <script type="text/javascript" src="/js/lib/onsenui/onsenui.min.js"></script>
     <script type="text/javascript" src="/js/lib/onsenui/angular-onsenui.min.js"></script>
