d4dc83df47b641b42d2d6c035dae3aa4e664cb5a
igara <igara1119@gmail.com>
2016-02-02 22:46:30 +0900

    routeでは配列の参照からurlパターンを当てるようにしない

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,65 +1,28 @@
 <?php
 
-use App\Http\Consts\UrlInfo;
-
-/*
-  |--------------------------------------------------------------------------
-  | Application Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register all of the routes for an application.
-  | It's a breeze. Simply tell Laravel the URIs it should respond to
-  | and give it the controller to call when that URI is requested.
-  |
- */
-
-// UrlInfoクラスをインスタンス化したものを取得する
-$urlInfo = new UrlInfo();
-
-// rootページのurl情報
-$rootUrlInfo = $urlInfo->getRootUrlInfo();
-
-// tabbarのurl情報
-$tabbarUrlInfo = $urlInfo->getTabbarUrlInfo();
-
-// sidemenuのurl情報
-$sidemenuUrlInfo = $urlInfo->getSideMenuUrlInfo();
-
-// homeタブのurl情報
-$homeTabUrlInfo = $urlInfo->getHomeTabUrlInfo();
-
-// contentsタブのurl情報
-$contentsTabUrlInfo = $urlInfo->getContentsTabUrlInfo();
-
-// docsタブのurl情報
-$docsTabUrlInfo = $urlInfo->getDocsTabUrlInfo();
-
-// devタブのurl情報
-$devTabUrlInfo = $urlInfo->getDevTabUrlInfo();
-
 // rootページ
-Route::get($rootUrlInfo['TOP']['URL'], 'Root\RootController@topAction');
-Route::get($rootUrlInfo['HOME']['URL'], 'Root\RootController@homeAction');
-Route::get($rootUrlInfo['CONTENTS']['URL'], 'Root\RootController@contentsAction');
-Route::get($rootUrlInfo['DOCS']['URL'], 'Root\RootController@docsAction');
-Route::get($rootUrlInfo['DEV']['URL'], 'Root\RootController@devAction');
+Route::get('/', 'Root\RootController@topAction');
+Route::get('/home', 'Root\RootController@homeAction');
+Route::get('/contents', 'Root\RootController@contentsAction');
+Route::get('/docs', 'Root\RootController@docsAction');
+Route::get('/dev', 'Root\RootController@devAction');
 
 // タブバー
-Route::get($tabbarUrlInfo['TABBAR']['URL'], 'Tabbar\TabbarController@tabbarAction');
+Route::get('/tabbar/tabbar', 'Tabbar\TabbarController@tabbarAction');
 
 // サイドメニュー
-Route::get($sidemenuUrlInfo['SIDEMENU']['URL'], 'SideMenu\SideMenuController@topAction');
+Route::get('/sidemenu/sidemenu_top', 'SideMenu\SideMenuController@topAction');
 
 // homeタブ
-Route::get($homeTabUrlInfo['TOP']['URL'], 'Tabbar\Navigation\Home\HomeController@topAction');
+Route::get('/tabbar/navigation/home/home_top', 'Tabbar\Navigation\Home\HomeController@topAction');
 
 // contentsタブ
-Route::get($contentsTabUrlInfo['TOP']['URL'], 'Tabbar\Navigation\Contents\ContentsController@topAction');
-Route::get($contentsTabUrlInfo['CHAT']['URL'], 'Tabbar\Navigation\Contents\ContentsController@chatAction');
+Route::get('/tabbar/navigation/contents/contents_top', 'Tabbar\Navigation\Contents\ContentsController@topAction');
+Route::get('/tabbar/navigation/contents/contents_chat', 'Tabbar\Navigation\Contents\ContentsController@chatAction');
 
 // docsタブ
-Route::get($docsTabUrlInfo['TOP']['URL'], 'Tabbar\Navigation\Docs\DocsController@topAction');
-Route::get($docsTabUrlInfo['SCREENLIST']['URL'], 'Tabbar\Navigation\Docs\DocsController@screenListAction');
+Route::get('/tabbar/navigation/docs/docs_top', 'Tabbar\Navigation\Docs\DocsController@topAction');
+Route::get('/tabbar/navigation/docs/screenlist', 'Tabbar\Navigation\Docs\DocsController@screenListAction');
 
 // devタブ
-Route::get($devTabUrlInfo['TOP']['URL'], 'Tabbar\Navigation\Dev\DevController@topAction');
+Route::get('/tabbar/navigation/dev/dev_top', 'Tabbar\Navigation\Dev\DevController@topAction');
