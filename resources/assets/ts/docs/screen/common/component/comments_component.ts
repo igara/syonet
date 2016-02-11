bc6485ee0f645da3fcb536df84e6ed055eec4c9a
五十嵐翔 <igara1119@gmail.com>
2016-02-11 15:22:47 +0900

    不要なファイルの削除

--- resources/assets/ts/docs/screen/common/component/comments_component.ts
+++
@@ -1,9 +0,0 @@
-import {Component, View} from 'angular2/core';
-import {bootstrap}    from 'angular2/platform/browser'
-
-@Component({
-    selector: 'comments_content',
-    template: `../../common/design/navigation/comments.html`
-})
-export class CommentsComponent {}
-bootstrap(CommentsComponent);
