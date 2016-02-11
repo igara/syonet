62f6cf32b5cfd9aaf95efa258732e234d88dfa4d
五十嵐翔 <igara1119@gmail.com>
2016-02-11 18:32:29 +0900

    脆弱性対応

--- public/index.php
+++ public/index.php
@@ -48,6 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 */
 
 header("X-Frame-Options: DENY");
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
