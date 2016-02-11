7e0dd613ded82b59e9ed70c0d985ad0b511b4c11
五十嵐翔 <igara1119@gmail.com>
2016-02-11 18:46:50 +0900

    脆弱性対応

--- public/index.php
+++ public/index.php
@@ -50,6 +50,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 header("X-Frame-Options: DENY");
 ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 1);
+header("Cache-Control: no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
