8de8f8930a754aecce97ac332e952a7f0286d372
五十嵐翔 <igara1119@gmail.com>
2016-03-07 22:18:48 +0900

    syonetに用いられる変数・関数を型定義ファイル化

---
+++ typings/syonet/BrowserSetting.d.ts
@@ -0,0 +1,16 @@
+/**
+ * ブラウザの大きさを取得する
+ * @return {
+ *          'width':number 横幅,
+ *          'height':number 縦幅
+ *         }
+ */
+export declare function getBrowserSize(): {
+    'width': number;
+    'height': number;
+};
+/**
+ * スリープの時間を取得する
+ * @return number スリープする時間（ms）
+ */
+export declare function getSleepTime(): number;
