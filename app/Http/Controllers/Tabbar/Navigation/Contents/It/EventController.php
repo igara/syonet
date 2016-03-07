a8e2c3c3e41bfd07ea24b28d2b9a5ee7f6edc4ac
五十嵐翔 <igara1119@gmail.com>
2016-03-07 23:35:23 +0900

    #68 connpassページ

---
+++ app/Http/Controllers/Tabbar/Navigation/Contents/It/EventController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Tabbar\Navigation\Contents\It;
+
+use App\Http\Controllers\Controller;
+use App\Http\Services\Contents\It\Event\ConnpassEventService;
+use Illuminate\Http\Request;
+
+/**
+ * EventController.
+ * 
+ * contentsタブ内で表示するコンテンツとして
+ * ITのEventのコンテンツを扱う
+ */
+class EventController extends Controller
+{
+    /**
+     * Create a new event controller instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * topアクション.
+     *
+     * @return view contents/it/event_topテンプレート
+     */
+    public function topAction()
+    {
+        return view('tabbar/navigation/contents/contents_top');
+    }
+
+    /**
+     * connpassアクション.
+     *
+     * @return view contents/it/event/connpassテンプレート
+     */
+    public function connpassAction(Request $request)
+    {
+        $connpassEventEvent = new ConnpassEventService();
+        $getConnpassOneMonthEvent = $connpassEventEvent->getConnpassApiOneMonthEventJson($request);
+        return view(
+            'tabbar/navigation/contents/it/event/connpass',
+            compact('getConnpassOneMonthEvent')
+        );
+    }
+}
---
+++ app/Http/Services/Contents/It/Event/ConnpassEventService.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Services\Contents\It\Event;
+
+/**
+ * ConnpassEventService.
+ *
+ * ConnpassEventを扱うService
+ */
+class ConnpassEventService
+{
+    /**
+     * Create a new instance.
+     */
+    public function __construct()
+    {
+    }
+    
+    /**
+     * ConnpassAPIにリクエストするURLの生成
+     * @param リクエストパラム
+     * @return string ConnpassAPIにリクエストするURL
+     */
+    private function _createRequestConnpassApiUrl($param) {
+        $year = $param['year'];
+        $month = $param['month'];
+        $count = $param['count'];
+        $firstMonth = date("$year-$month-01");
+        $lastMonth = date("$year-$month-t");
+
+        return "http://connpass.com/api/v1/event/?keyword=python";
+    }
+
+    /**
+     * connpassのAPIからJSONを取得する.
+     * 
+     * @param array リクエストパラム
+     * @return array connpassのイベントを取得
+     */
+    public function getConnpassApiOneMonthEventJson($param)
+    {   
+        $context = stream_context_create([
+           'http' => array('ignore_errors' => true)
+        ]);
+        $response = file_get_contents(
+            $this->_createRequestConnpassApiUrl($param),
+            false,
+            $context
+        );
+
+        // ステータスコードを取得する
+        preg_match(
+            '/HTTP\/1\.[0|1|x] ([0-9]{3})/',
+            $http_response_header[0],
+            $matches
+        );
+        $status_code = $matches[1];
+        
+        // APIのデータ取得用変数
+        $eventData = [];
+
+        switch ($status_code) {
+            case '200':
+                // JSONファイルは配列に変換しておく
+                $jsonData = mb_convert_encoding($response, 'UTF8', 'auto');
+                $eventData = json_decode($jsonData, true);
+                break;
+            case '403';
+                // 403の場合
+                $eventData = [
+                    'status' => '403'
+                ];
+                break;
+            case '404':
+                // 404の場合
+                $eventData = [
+                    'status' => '404'
+                ];
+                break;
+            default:
+                $eventData = [
+                    'status' => 'error'
+                ];
+                break;
+        }
+        return $eventData;
+    }
+}
