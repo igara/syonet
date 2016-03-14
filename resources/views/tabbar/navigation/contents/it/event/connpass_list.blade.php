fe10dbdd01290cdd2deaf87c7ca63fcacc6b8b37
五十嵐翔 <igara1119@gmail.com>
2016-03-14 23:39:46 +0900

    イベント開催日時の項目追加

--- resources/views/tabbar/navigation/contents/it/event/connpass_list.blade.php
+++ resources/views/tabbar/navigation/contents/it/event/connpass_list.blade.php
@@ -1,7 +1,7 @@
 <form action="" method="get">
     <input type="hidden" name="contents" value="connpass">
-    <input type="text" class="text-input" placeholder="@{{$ctrl.year}}" name="year" style="width: 50px;">年
-    <input type="text" class="text-input" placeholder="@{{$ctrl.month}}" name="month" style="width: 50px;">月
+    <input type="text" class="text-input" placeholder="@{{$ctrl.yearParam}}" name="year" style="width: 50px;">年
+    <input type="text" class="text-input" placeholder="@{{$ctrl.monthParam}}" name="month" style="width: 50px;">月
     <input type="text" class="search-input" placeholder="keyword (例)swift" name="keyword" style="width: 200px;">
     <input type="submit" class="button button--light" value="検索">
 </form>
@@ -17,11 +17,23 @@
                         @{{connpassCasset.title}}
                     </a>
                 </div>
+                <div class="day">
+                     <ul>
+                         <li>開始日時：@{{$ctrl.getFormatedData(connpassCasset.started_at)}}</li>
+                         <li>終了日時：@{{$ctrl.getFormatedData(connpassCasset.ended_at)}}</li>
+                     </ul>
+                </div>
+                <div class="join">
+                     定員：@{{connpassCasset.accepted}}／@{{connpassCasset.limit}}（補欠：@{{connpassCasset.waiting}}）
+                </div>
                 <div class="location">
                     <i class="fa fa-map-marker"></i> @{{connpassCasset.address}}
                 </div>
                 <div class="desc">
                     @{{connpassCasset.place}}
+                </div>
+                <div class="hash">
+                    ハッシュタグ：@{{connpassCasset.hash_tag}}
                 </div>
             </ons-col>
         </ons-row>
