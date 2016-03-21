20d7ce8802d472391cfef5a29fb4aecb8142c4b7
五十嵐翔 <igara1119@gmail.com>
2016-03-21 14:59:57 +0900

    サービスのクラスを作成 リクエストパラメータのインターフェースを作成

--- resources/assets/ts/contents/components/connpass.ts
+++ resources/assets/ts/contents/components/connpass.ts
@@ -1,74 +1,15 @@
 /// <reference path="../../../../../typings/main.d.ts" />
 /// <reference path="../../../../../typings/syonet/SyonetWork.d.ts" />
 
-/**
- * Connpassを扱うコントローラ
- */
-class ConnpassCtrl {
-    // Http通信を扱うサービス
-    http: ng.IHttpService;
-    // locationを扱うサービス
-    locationService:ng.ILocationService;
-    // JSONデータを扱う
-    getJson:any;
-    // 年月を取得する 
-    yearParam:any;
-    monthParam:any;
-    // 検索開始位置
-    startParam:any;
-    // 検索出力数
-    countParam:any;
-    // ページ
-    pageParam:any;
-    // Math関数
-    Math:Math;
-    // キーワード
-    keywordParam:any;
-
-    /**
-     * コンストラクタ
-     */
-    constructor($http: ng.IHttpService, $location:ng.ILocationService) {
-        this.http = $http;
-        this.locationService = $location;
-        this.Math = Math;
-        // 年月を取得する 
-        this.yearParam = this.getYearParam();
-        this.monthParam = this.getMonthParam();
-        // 検索開始位置
-        this.startParam = this.getStartParam();
-        // 検索出力数
-        this.countParam = this.getCountParam();
-        // ページ
-        this.pageParam = this.getPageParam();
-        var start = this.getPageInfo().start;
-        var count = this.getPageInfo().count;
-        
-        // URLパラメータから検索ワードを取得する
-        this.keywordParam = this.getKeywordParam();
-
-        this.callConnpassApiJson(this.keywordParam, this.yearParam, this.monthParam, start.toString(), count);
-    }
-
-    /**
-     * ConnpassAPIから情報を取得する
-     */
-    callConnpassApiJson(keyword:string, year:string, month:string, start:string, count:string) {
-        this.http.jsonp(`//connpass.com/api/v1/event/?keyword=${keyword}&count=${count}&start=${start}&ym=${year}${month}&callback=JSON_CALLBACK`)
-        .success((data, status) => {
-            this.getJson = data;
-        }).error(error => {
-            this.getJson = error;
-        });
-    }
-
+class ConnpassService {
     /**
      * 検索する時の年を取得する
+     * @param ng.ILocationService locationService
      * @return number 年
      */
-    getYearParam() {
+    getYearParam(locationService:ng.ILocationService) {
         // URLパラメータから年を取得する
-        var year = this.locationService.search(location.search).search()["year"];
+        var year = locationService.search(location.search).search()["year"];
         // パラメータのyearがundefinedもしくは数字ではない場合今年
         if (year === "" || year === undefined || !isFinite(year)) {
             year = new Date().getFullYear();
@@ -78,11 +19,12 @@ class ConnpassCtrl {
 
     /**
      * 検索する時の月を取得する
+     * @param ng.ILocationService locationService
      * @return number 月
      */
-    getMonthParam() {
+    getMonthParam(locationService:ng.ILocationService) {
         // URLパラメータから月を取得する
-        var month = this.locationService.search(location.search).search()["month"];
+        var month = locationService.search(location.search).search()["month"];
         // パラメータのmonthがundefinedもしくは数字ではない場合今月
         if (month === "" || month === undefined || !isFinite(month)) {
             month = new Date().getMonth() + 1;
@@ -96,11 +38,12 @@ class ConnpassCtrl {
 
     /**
      * 検索する時の検索開始位置を取得する
+     * @param ng.ILocationService locationService
      * @return number 検索開始位置
      */
-    getStartParam() {
+    getStartParam(locationService:ng.ILocationService) {
         // URLパラメータから検索開始位置を取得する
-        var start = this.locationService.search(location.search).search()["start"];
+        var start = locationService.search(location.search).search()["start"];
         // パラメータのstartがundefinedもしくは数字ではない場合1
         if (start === undefined || !isFinite(start)) {
             start = 1;
@@ -110,52 +53,62 @@ class ConnpassCtrl {
 
     /**
      * 検索する時の検索出力数を取得する
+     * @param ng.ILocationService locationService
      * @return number 検索出力数
      */
-    getCountParam() {
+    getCountParam(locationService:ng.ILocationService) {
         // URLパラメータから検索出力数を取得する
-        var count = this.locationService.search(location.search).search()["count"];
+        var count = locationService.search(location.search).search()["count"];
         // パラメータのcountがundefinedもしくは数字ではない場合20
         if (count === undefined || !isFinite(count)) {
             count = 20;
         }
         return count;
     }
+
     /**
      * ページを取得する
+     * @param ng.ILocationService locationService
      * @return number ページ
      */
-    getPageParam() {
+    getPageParam(locationService:ng.ILocationService) {
         // URLパラメータから検索開始位置を取得する
-        var page = this.locationService.search(location.search).search()["page"];
+        var page = locationService.search(location.search).search()["page"];
         // パラメータのstartがundefinedもしくは数字ではない場合1
         if (page === undefined || !isFinite(page)) {
             page = 1;
         }
         return page;
     }
+
     /**
      * ページに対する情報を取得する
-     * @return number ページの情報
+     * @param ng.ILocationService locationService
+     * @return {
+     *     'start':number 検索結果の読み込み開始位置,
+     *     'count':number 読み込む検索結果の件数
+     * }
      */
-    getPageInfo() {
-        var start = this.startParam;
-        var count = this.countParam;
-        if (this.pageParam > 1) {
-            start = (this.pageParam - 1) * this.countParam + 1;
+    getPageInfo(locationService:ng.ILocationService) {
+        var start = this.getStartParam(locationService);
+        var pageParam = this.getPageParam(locationService);
+        var countParam = this.getCountParam(locationService);
+        if (pageParam > 1) {
+            start = (pageParam - 1) * countParam + 1;
         }
         return {
             'start':start,
-            'count':this.countParam
+            'count':countParam
         };
     }
 
     /**
      * 検索する時のキーワードを取得する
+     * @param ng.ILocationService locationService
      * @return string キーワード
      */
-    getKeywordParam() {
-        return this.locationService.search(location.search).search()["keyword"];
+    getKeywordParam(locationService:ng.ILocationService) {
+        return locationService.search(location.search).search()["keyword"];
     }
 
     /**
@@ -163,7 +116,7 @@ class ConnpassCtrl {
      * @param any 日付
      * @return string 'XXXX/XX/XX XX:XX:XX'
      */
-    getFormatedData(dataTime:any) {
+    getFormatedDate(dataTime:any) {
         var date:Date = new Date(dataTime);
         // paramから年を取得する
         var year:any = date.getFullYear();
@@ -209,6 +162,70 @@ class ConnpassCtrl {
         var arr:any = [];
         for (var i = 1; i <= resultCount; ++i) arr.push(i);
         return arr;
+    }
+
+    /**
+     * ConnpassAPIから情報を取得する
+     * @param ng.IHttpService http,
+     *        ConnpassApiParams connpassApiParams ConnpassAPIに問い合わせるときに用いるparam
+     * @return http.jsonp APIのレスポンス
+     */
+    callConnpassApiJson(http:ng.IHttpService, connpassApiParams:ConnpassApiParams) {
+        return http.jsonp(`//connpass.com/api/v1/event/?keyword=${connpassApiParams.keyword}&count=${connpassApiParams.count}&start=${connpassApiParams.start}&ym=${connpassApiParams.year}${connpassApiParams.month}&callback=JSON_CALLBACK`);
+    }
+}
+
+/**
+ * ConnpassAPIを使用するときに用いるリクエストパラメータ
+ */
+interface ConnpassApiParams {
+    keyword:string;
+    year:string;
+    month:string;
+    start:string;
+    count:string;
+}
+
+/**
+ * Connpassを扱うコントローラ
+ */
+class ConnpassCtrl {
+    // Http通信を扱うサービス
+    http:ng.IHttpService;
+    // locationを扱うサービス
+    locationService:ng.ILocationService;
+    // JSONデータを扱う
+    getJson:any;
+    // Math関数
+    Math:Math;
+    // ConnpassService
+    connpassService:ConnpassService;
+    // ConnpassAPIで使用されるparam
+    connpassApiParams:ConnpassApiParams;
+
+    /**
+     * コンストラクタ
+     */
+    constructor($http: ng.IHttpService, $location:ng.ILocationService) {
+        this.http = $http;
+        this.locationService = $location;
+        this.Math = Math;
+        this.connpassService = new ConnpassService();
+
+        this.connpassApiParams = {
+            keyword:this.connpassService.getKeywordParam(this.locationService),
+            year:this.connpassService.getYearParam(this.locationService),
+            month:this.connpassService.getMonthParam(this.locationService),
+            start:this.connpassService.getPageInfo(this.locationService).start,
+            count:this.connpassService.getPageInfo(this.locationService).count
+        };
+
+        var callConnpassApi = this.connpassService.callConnpassApiJson(this.http, this.connpassApiParams);
+        callConnpassApi.success((data, status) => {
+            this.getJson = data;
+        }).error(error => {
+            this.getJson = error;
+        });
     }
 }
 
--- resources/views/tabbar/navigation/contents/it/event/connpass_list.blade.php
+++ resources/views/tabbar/navigation/contents/it/event/connpass_list.blade.php
@@ -1,6 +1,6 @@
-<span ng-repeat="count in $ctrl.getCountArray($ctrl.getJson['results_available'])" style="display:inline;">
-    <a href="/contents/?contents=connpass&year=@{{$ctrl.yearParam}}&month=@{{$ctrl.monthParam}}&keyword=@{{$ctrl.keywordParam}}&page=@{{(count + 1) / $ctrl.countParam}}" ng-if="(count + 1) % $ctrl.countParam == 0">@{{(count + 1) / $ctrl.countParam}} </a>
-    <a href="/contents/?contents=connpass&year=@{{$ctrl.yearParam}}&month=@{{$ctrl.monthParam}}&keyword=@{{$ctrl.keywordParam}}&page=@{{$ctrl.Math.ceil($ctrl.getJson['results_available'] / $ctrl.countParam)}}" ng-if="$ctrl.getJson['results_available'] > $ctrl.countParam && count + 1 == $ctrl.getJson['results_available'] && $ctrl.getJson['results_available'] % $ctrl.countParam != 0">@{{$ctrl.Math.ceil($ctrl.getJson['results_available'] / $ctrl.countParam)}}</a>
+<span ng-repeat="count in $ctrl.connpassService.getCountArray($ctrl.getJson['results_available'])" style="display:inline;">
+    <a href="/contents/?contents=connpass&year=@{{$ctrl.connpassApiParams.year}}&month=@{{$ctrl.connpassApiParams.month}}&keyword=@{{$ctrl.connpassApiParams.keyword}}&page=@{{(count + 1) / $ctrl.connpassApiParams.count}}" ng-if="(count + 1) % $ctrl.connpassApiParams.count == 0">@{{(count + 1) / $ctrl.connpassApiParams.count}} </a>
+    <a href="/contents/?contents=connpass&year=@{{$ctrl.connpassApiParams.year}}&month=@{{$ctrl.connpassApiParams.month}}&keyword=@{{$ctrl.connpassApiParams.keyword}}&page=@{{$ctrl.Math.ceil($ctrl.getJson['results_available'] / $ctrl.connpassApiParams.count)}}" ng-if="$ctrl.getJson['results_available'] > $ctrl.connpassApiParams.count && count + 1 == $ctrl.getJson['results_available'] && $ctrl.getJson['results_available'] % $ctrl.connpassApiParams.count != 0">@{{$ctrl.Math.ceil($ctrl.getJson['results_available'] / $ctrl.connpassApiParams.count)}}</a>
 </span>
 <li class="list__header" ng-if="$ctrl.getJson['results_returned']">
     @{{$ctrl.getJson['results_start']}}〜@{{$ctrl.getJson['results_start'] -1 + $ctrl.getJson['results_returned']}}件 (該当件数：@{{$ctrl.getJson['results_available']}}件)
@@ -16,8 +16,8 @@
                 </div>
                 <div class="day">
                      <ul>
-                         <li>開始日時：@{{$ctrl.getFormatedData(connpassCasset.started_at)}}</li>
-                         <li>終了日時：@{{$ctrl.getFormatedData(connpassCasset.ended_at)}}</li>
+                         <li>開始日時：@{{$ctrl.connpassService.getFormatedDate(connpassCasset.started_at)}}</li>
+                         <li>終了日時：@{{$ctrl.connpassService.getFormatedDate(connpassCasset.ended_at)}}</li>
                      </ul>
                 </div>
                 <div class="join">
